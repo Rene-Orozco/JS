563ac76bcb8da2bccd32cbadd271af56b59d53cb
rene-orozco <oroozco.rene@gmail.com>
2025-04-21 17:30:27 -0500

    actualizado

---
+++ 00_holaMundo/app.js
@@ -0,0 +1,13 @@
+// esto es uncomentario de una linea
+
+
+/* 
+esto es un comentario 
+de varias linea de codigo
+*/
+
+
+// metodo para imprimir en pantalla 
+console.log("Hola  mundo");
+
+
---
+++ 01_Variables/app.js
@@ -0,0 +1,25 @@
+// creamos variables de forma literal o primitiva
+
+//! primera forma no aconsejable
+valor = 5;
+
+//! segunda forma no aconsejable
+// rescribir    reasignando
+var otraValor = "hola"; /// 1
+otraValor = 5;
+
+
+//  redifiniendo
+var otraValor = "nombre";
+
+// -----------------------------------------
+//* aconsejable varibales
+let nombre = "rose";
+console.log(nombre);
+
+nombre = 48;
+console.log(nombre);
+
+
+//* constante
+const numero = 78;
