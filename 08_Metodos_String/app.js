f9330e4fda53b23342210f764c8a009a3f7b541a
rene-orozco <oroozco.rene@gmail.com>
2025-04-25 15:09:36 -0500

    actualizacion

---
+++ 08_Metodos_String/app.js
@@ -0,0 +1,11 @@
+/* 
+algunos Metodos de los Strings
+funciones nativas o metodo incorporados, viene incluida en el lenguaje 
+sin que tengas que escribir codigo desde cero
+
+en pocas palabras son funciones que se deben usar si o si 
+como dice o lo propone el lenguaje 
+*/
+
+
+
