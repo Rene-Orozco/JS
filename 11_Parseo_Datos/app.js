1e15bf63c1e9815bee5a13154347642625742c12
rene-orozco <oroozco.rene@gmail.com>
2025-05-07 16:41:00 -0500

    actualizado

--- 11_Parseo_Datos/app.js
+++ 11_Parseo_Datos/app.js
@@ -1,34 +1,57 @@
 // ultimo tema de JS bascio
 
 // parseo de datos, conversion de datos, refundicion de datos
-let valor =  "124.5";
-console.log(typeof(valor));
+let valor = "124.535434";
+console.log(typeof (valor));
 
 
 // forma manual 
-let datoConvertido = parseInt(valor);
+// convierta en numero entero, perdida de datos
+let dato = parseInt(valor);
+console.log(dato);
+console.log(typeof(dato));
+
+// convierte en numero flotante, no hay perdida de datos
+let dato2 = parseFloat(valor);
+console.log(dato2);
+console.log(typeof(dato2));
+
+
+// -----------------------
+// -----------------------
+// forma dinamica
+let numero = "78.4643847";
+let datoConvertido = Number(numero);
+// let datoConvertido = +numero;
 console.log(datoConvertido);
 console.log(typeof(datoConvertido));
 
-let datoFloat = parseFloat(valor);
-console.log(datoFloat);
-console.log(typeof(datoFloat));
 
 
-// forma dinamica
-let datoNumber = Number(valor);
-console.log(datoNumber);
-console.log(typeof(datoNumber));
-
-let datoPlus = +valor;
-console.log(datoPlus);
-console.log(typeof(datoPlus));
-
-
-// de numero a texto 
-let numeroTexto = 32545;
+//  pasamos un valor numerico a tipo texto
+let numeroGuardado = 57845.13;
+console.log(typeof(numeroGuardado));
+let numeroTexto = numeroGuardado.toString();
+console.log(numeroTexto);
 console.log(typeof(numeroTexto));
-console.log(typeof(numeroTexto.toString()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
